67980c543c7782f1d1f03676a5aa77bf35347acd
e <2528877987@qq.com>
2019-06-03 15:33:21 +0800

    #新增机台开工率模块

--- mini-program-f2-demos-master/pages/sjindex/sjindex.js
+++ mini-program-f2-demos-master/pages/sjindex/sjindex.js
@@ -1,16 +1,13 @@
 Page({
   data: {
     charts: [
-      // { name: 'ksh', value: '销售报表' },
       { name: '10001', value: '销售趋势' },
       { name: '10002', value: '库存查询' },
       { name: '10003', value: '空间布销售' },
       { name: '10004', value: '洋葱报表' },
-      { name: '10005', value: '生产情况' },
-      // { name: 'ooopic_1554780361', value: '销售报表' },
-      // { name: 'ooopic_1554780362', value: '销售报表' },
-      // { name: 'ooopic_1554780363', value: '销售报表' },
-      // { name: 'ooopic_1554780364', value: '销售报表' },
+    ],
+    charts1: [
+      { name: '10005', value: '机台开工率' },
     ],
   },
   gotoPage: function (e) {
@@ -35,16 +32,14 @@ Page({
         url: '../xsbb/YCbb/YCbb'
       });
     }
+  },
+  gotoPage1: function (e) {
+    var page = e.currentTarget.dataset.page;
     if (page == '10005') {
       my.navigateTo({
         url: '../xsbb/scqk/scqk'
       });
     }
-    // else {
-    //   my.navigateTo({
-    //     url: '../xsbb/line/index'
-    //   });
-    // }
   },
   onLoad: function () {
   }
